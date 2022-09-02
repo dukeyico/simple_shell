ad455ab65737cd113f0c07e300fa5f4ca5effca6
Marvellouschris <marvellousekhatorik@gmail.com>
2022-09-02 00:04:21 -0700

    pointer to the created token

--- str_token.c
+++ str_token.c
@@ -4,6 +4,7 @@
  * @line: It´s pointer to array we receive in getline.
  * @delim: It´s characters we mark off string in parts.
  * Return: A pointer to the created token
+ * updated
 */
 char *_strtok(char *line, char *delim)
 {
